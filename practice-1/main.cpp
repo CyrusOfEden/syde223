d8612ccf66dd3e116243d7435edf5f2d6a92b7b8
alexdales <contact@alexdales.com>
2020-01-09 15:49:37 -0500

    Inherited Music to Song, created constructors.

--- practice-1/main.cpp
+++ practice-1/main.cpp
@@ -4,19 +4,15 @@
 
 using namespace std;
 
-class Artwork {
-};
-
 class Music {
 public:
-    Artwork artwork;
     const string id;
     const string artist_name;
     unsigned int year;
 
     Music() : id(""), artist_name(""), year(0) {};
 
-    Music(string &_id, string &_artist, unsigned int _year) : id(_id), artist_name(_artist), year(_year) {};
+    Music(string _id, string _artist, unsigned int _year) : id(_id), artist_name(_artist), year(_year) {};
 
     string get_artist() {
         return artist_name;
@@ -27,7 +23,25 @@ public:
     };
 };
 
-class Song {
+// requires: genre, song name, song length (unsigned int)
+class Song : Music {
+public:
+    const string genre;
+    const string song_name;
+    unsigned int song_length;
+
+    Song() : genre(""), song_name(""), song_length(0) {
+        super();
+    };
+
+    Song(string _genre, string _song_name, unsigned int _song_length) : genre(_genre), song_name(_song_name),
+                                                                        song_length(_song_length) {};
+
+    bool operator==(Song &s) {
+        return s.id == id;
+    };
+};
+
 };
 
 class Playlist {
