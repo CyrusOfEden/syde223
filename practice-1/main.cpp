5c89e2a8468d9736c12cf8a851d9e013aad4866a
Kash Nouroozi <knouroozi@gmail.com>
2020-01-09 14:57:14 -0500

    Music class

--- practice-1/main.cpp
+++ practice-1/main.cpp
@@ -1,4 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Artwork {
+};
+
+class Music {
+public:
+    Artwork artwork;
+    const string id;
+    const string artist_name;
+    unsigned int year;
+
+    Music() : id(""), artist_name(""), year(0) {};
+
+    Music(string &_id, string &_artist, unsigned int _year) : id(_id), artist_name(_artist), year(_year) {};
+
+    string get_artist() {
+        return artist_name;
+    };
+
+    bool operator==(Music &m) {
+        return m.id == id;
+    };
+};
+
+class Song {
+};
+
+class Playlist {
+};
 
 int main() {
     srand(time(0));
